3d61782da6b918eba6151e40caf28f1f6c59a2a2
ckyle121 <carolinekyle21@gmail.com>
2022-09-05 14:48:11 -0500

    we're using scss now

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,39 +2,55 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Navigation = () => {
   return (
-    <>
-      <Navbar
-        className="navbar"
-        bg="primary"
-        variant="dark"
-        expand="lg"
-        sticky="top"
-      >
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            Trop Rock'n Christmas
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/home">
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <p>Trop Rock'n Christmas</p>
+      </div>
+      <ul className="app__navbar-links">
+          <Nav.Link as={Link} to="/home"  className="app__flex p-text">
                 Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/cookie">
-                Cookie
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about">
-                About
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/cookie"  className="app__flex p-text">
+                Cookies
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about"  className="app__flex p-text">
+                About Us
+          </Nav.Link>
+       
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
   );
 };
 
