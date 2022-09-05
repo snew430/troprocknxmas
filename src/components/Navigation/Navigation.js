f3dea789a6697f31ea7caac5849232848a6568ef
ckyle121 <carolinekyle21@gmail.com>
2022-09-05 16:33:04 -0500

    nav bar menu styled

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -30,33 +30,34 @@ const Navigation = () => {
       </ul>
 
       <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              <li className="app__flex p-text">
-                <Link as={Link} to="/home"  className="app__flex p-text">
-                    Home
-                </Link>
-              </li>
-              <li className="app__flex p-text">
-                <Link as={Link} to="/cookie"  className="app__flex p-text">
-                    Cookies
-                </Link>
-              </li>
-              <li className="app__flex p-text">
-                <Link as={Link} to="/about"  className="app__flex p-text">
-                    About Us
-                </Link>
-              </li>
-              </ul>
-          </motion.div>
-        )}
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+          {toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                <li className="app__flex p-text">
+                  <Link as={Link} to="/home"  className="app__flex p-text">
+                      Home
+                  </Link>
+                </li>
+                <li className="app__flex p-text">
+                  <Link as={Link} to="/cookie"  className="app__flex p-text">
+                      Cookies
+                  </Link>
+                </li>
+                <li className="app__flex p-text">
+                  <Link as={Link} to="/about"  className="app__flex p-text">
+                      About Us
+                  </Link>
+                </li>
+                </ul>
+            </motion.div>
+          )}
+
       </div>
     </nav>
   );
