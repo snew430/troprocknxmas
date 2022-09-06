f3bec3d6a5ccd7d352af70e8c9aebbc87f6d6b3a
ckyle121 <carolinekyle21@gmail.com>
2022-09-06 15:08:35 -0500

    still trying to figure out the home page

--- src/components/Cookie/Cookie.js
+++ src/components/Cookie/Cookie.js
@@ -8,27 +8,25 @@ import Footer from '../Footer/Footer';
 const Cookie = () => {
   return (
     <div id="cookie">
-      <h2 className='head-text'>Ahhhh.... The Cookies</h2>
-      <p className='info-text'>You can support our fundraiser by buying cookies!</p>
-      <img src={cookie} alt="cookie" />
-      <p className='info-text'>
-        The cookies are sold in quantities of{' '}
-        <span className="emphasize">1 pound.</span>
-      </p>
-      <p>
-        All of the proceeds will go to{' '}
-        <span className="emphasize">“Remember Our Children”</span>. 
-        <br />
-        Their mission is to help parents who have lost children to tragedy. Here is a
-        link to their web site.
-      </p>
-      <a href="http://www.rocpark.com/" rel="noreferrer" target="_blank">
-        <span className="emphasize">R.O.C. Park</span>
-      </a>
-      <img src={baking} alt="joe baking cookies"/>
-
-      <p className='info-text'>More information to come on how you can support this fundraiser!</p>
-      <Footer sticky="bottom" />
+      <div className='background'>
+        <h2 className='head-text'>Ahhhh.... The Cookies</h2>
+        <p className='info-text'>You can support our fundraiser by buying cookies!</p>
+        <img src={cookie} alt="cookie" />
+        <img src={baking} alt="joe baking cookies"/>
+        <p className='info-text'>
+          Cookies are sold by the pound, and all of the proceeds go to the organization:
+          <br/>
+        
+          “Remember Our Children.”
+          <br />
+          Their mission is to help parents who have lost children to tragedy.
+          <br/>
+          <a href="http://www.rocpark.com/" rel="noreferrer" target="_blank">Click Here to Visit the R.O.C. Website</a>
+          < br/>
+          More information to come on how you can support this fundraiser!
+        </p>
+        <Footer sticky="bottom" />
+      </div>
     </div>
   );
 };
