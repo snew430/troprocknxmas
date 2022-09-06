fda30e01e2a40957060c01060d984ee4305f64aa
ckyle121 <carolinekyle21@gmail.com>
2022-09-06 16:00:43 -0500

    that's all for now

--- src/info/aboutInfo.js
+++ src/info/aboutInfo.js
@@ -5,13 +5,13 @@ const info = [
   {
     title: 'John Patti',
     pic: john,
-    bio: 'Multi-award-winning journalist and longtime Baltimore broadcaster, John Patti is your host for "A Trop Rockin\' Christmas." John takes his microphone across the U.S. to bring you cherished holiday memories from some of your favorite Trop Rock artists and fans, as well as the Trop Rock Christmas music that we all love.',
+    bio: 'Multi-award-winning journalist and longtime Baltimore broadcaster, John Patti, is your host for "A Trop Rockin\' Christmas." John takes his microphone across the U.S. to bring you cherished holiday memories from some of your favorite Trop Rock artists and fans, as well as the Trop Rock Christmas music that we all love.',
     link: 'jvpatti@verizon.net',
   },
   {
     title: 'Joe Evelius',
     pic: joe,
-    bio: 'Veteran Maryland broadcaster Joe Evelius is the co-founder, producer and engineer responsible for the sound of "A Trop Rockin\' Christmas." He also serves as our Music Director.',
+    bio: 'Veteran Maryland broadcaster, Joe Evelius, is the co-founder, producer and engineer responsible for the sound of "A Trop Rockin\' Christmas." He also serves as our Music Director.',
     link: 'joeevelius@comcast.net',
   },
 ];
