ed22e92d1c12b8a436429ad239268db8ee291176
ckyle121 <carolinekyle21@gmail.com>
2022-09-06 15:39:07 -0500

    cookie paragraph

--- src/components/Cookie/Cookie.js
+++ src/components/Cookie/Cookie.js
@@ -13,18 +13,17 @@ const Cookie = () => {
         <p className='info-text'>You can support our fundraiser by buying cookies!</p>
         <img src={cookie} alt="cookie" />
         <img src={baking} alt="joe baking cookies"/>
-        <p className='info-text'>
-          Cookies are sold by the pound, and all of the proceeds go to the organization:
-          <br/>
-        
-          “Remember Our Children.”
-          <br />
-          Their mission is to help parents who have lost children to tragedy.
-          <br/>
-          <a href="http://www.rocpark.com/" rel="noreferrer" target="_blank">Click Here to Visit the R.O.C. Website</a>
-          < br/>
-          More information to come on how you can support this fundraiser!
-        </p>
+          <p className='charity-text'>
+            Cookies are sold by the pound, and all of the proceeds go to the organization:
+            <br/>
+            “Remember Our Children.”
+            <br />
+            Their mission is to help parents who have lost children to tragedy.
+            <br/>
+            <a href="http://www.rocpark.com/" rel="noreferrer" target="_blank">Click Here to Visit the R.O.C. Website</a>
+            <br/>
+            More information to come on how you can support this fundraiser!
+          </p>
         <Footer sticky="bottom" />
       </div>
     </div>
