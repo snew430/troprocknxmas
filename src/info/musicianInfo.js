53e15f3127d6cb7622b2d4eb4c668a7ced6557f9
ckyle121 <carolinekyle21@gmail.com>
2022-09-16 15:41:24 -0500

    map over musicians

---
+++ src/info/musicianInfo.js
@@ -0,0 +1,15 @@
+const musicians = [
+    'Brendan Mayer', 
+    'Tall Paul Bobal', 
+    'Sunny Jim White', 
+    'John Patti', 
+    'Jimmy & the Parrots', 
+    'Kelly McGuire', 
+    'Isabella Stefania', 
+    'Gary James Moeller', 
+    'Mike Broward', 
+    'Kenny Chesney', 
+    'Jimmy Buffett'
+]
+
+export default musicians; 
