58a11e085e2db2afe24d31ae2a5898104575b08f
snew430 <seanpnew@hotmail.com>
2022-11-01 09:47:37 -0400

    deleted nomatch in index

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import Home from './Home/Home';
 import Navigation from './Navigation/Navigation';
 import About from './About/About';
 import Cookie from './Cookie/Cookie';
-import NoMatch from './NoMatch/NoMatch';
 import Lights from './Lights/Lights';
 
-export { Footer, Home, Navigation, About, NoMatch, Cookie, Lights };
+export { Footer, Home, Navigation, About, Cookie, Lights };
